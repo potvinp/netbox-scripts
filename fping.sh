bc68ac1868b7e1fc92c27cb653b84d97abed3780
Peter Potvin <potvinp@users.noreply.github.com>
2021-02-15 13:22:44 -0500

    Fix fping output
    fping originally output results to stderr, which previous code resulted in no results being output to fping_hosts. This commit fixes this issue.

--- fping.sh
+++ fping.sh
@@ -1,5 +1,5 @@
+#!/usr/bin/env bash
 echo "127.0.0.1" > fping_hosts
-fping=`cat fping_prefixes` 
-for i in $fping; do
-   sudo fping -c 1 -r 0 -i 1 -a -g "%i" >> fping_hosts 
+for ping in $(cat fping_prefixes);
+  do sudo fping -c 1 -r 0 -i 1 -a -g "$ping" >> fping_hosts 2>&1
 done
